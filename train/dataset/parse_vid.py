7f7368cdf35d5a940b0dad27c6fc5081b8c1046d
Jannik Bamberger <dev.jbamberger@gmail.com>
2020-12-21 17:20:42 +0100

    parse_vid.py should now work on windows, too

--- train/dataset/parse_vid.py
+++ train/dataset/parse_vid.py
@@ -1,10 +1,14 @@
-from os.path import join, isdir
-from os import listdir
 import argparse
-import json
 import glob
+import json
+import os
 import xml.etree.ElementTree as ET
-import pdb
+from os import listdir
+from os.path import join
+
+from tqdm import tqdm
+
+OUT_FILE = 'vid.json'
 
 parser = argparse.ArgumentParser(description='Parse the VID Annotations for training DCFNet')
 parser.add_argument('data', metavar='DIR', help='path to VID')
@@ -12,36 +16,45 @@ args = parser.parse_args()
 
 print('VID2015 Data:')
 VID_base_path = args.data
-ann_base_path = join(VID_base_path, 'Annotations/VID/train/')
-img_base_path = join(VID_base_path, 'Data/VID/train/')
-sub_sets = sorted({'a', 'b', 'c', 'd', 'e'})
-# sub_sets = sorted({'ILSVRC2015_VID_train_0000', 'ILSVRC2015_VID_train_0001', 'ILSVRC2015_VID_train_0002', 'ILSVRC2015_VID_train_0003'})
+ann_base_path = join(VID_base_path, 'Annotations', 'VID', 'train')
+img_base_path = join(VID_base_path, 'Data', 'VID', 'train')
+# sub_sets = sorted({'a', 'b', 'c', 'd', 'e'})
+sub_sets = sorted({'ILSVRC2015_VID_train_0000',
+                   'ILSVRC2015_VID_train_0001',
+                   'ILSVRC2015_VID_train_0002',
+                   'ILSVRC2015_VID_train_0003'})
 total_frame = 0
 
 vid = []
-for sub_set in sub_sets:
-    sub_set_base_path = join(ann_base_path, sub_set)
-    videos = sorted(listdir(sub_set_base_path))
+for subset_name in sub_sets:
+    subset_path = join(ann_base_path, subset_name)
+    video_paths = sorted(listdir(subset_path))
+
+    print(f'Subset: {subset_name}')
+    vids = tqdm(enumerate(video_paths), desc='Processing videos:', total=len(video_paths))
+
     s = []
-    for vi, video in enumerate(videos):
-        print('subset: {} video id: {:04d} / {:04d}'.format(sub_set, vi, len(videos)))
-        v = dict()
-        v['base_path'] = join(img_base_path, sub_set, video)
-        v['frame'] = []
-        video_base_path = join(sub_set_base_path, video)
-        xmls = sorted(glob.glob(join(video_base_path, '*.xml')))
-        for xml in xmls:
+    for vi, video in vids:
+        v = {
+            'base_path': join(img_base_path, subset_name, video),
+            'frame': []
+        }
+        frame_annotation_paths = sorted(glob.glob(join(subset_path, video, '*.xml')))
+        for frame_annotation_path in frame_annotation_paths:
             total_frame += 1
-            f = dict()
-            xmltree = ET.parse(xml)
-            size = xmltree.findall('size')[0]
-            frame_sz = [int(it.text) for it in size]
-            f['frame_sz'] = frame_sz
-            f['img_path'] = xml.split('/')[-1].replace('xml', 'JPEG')
+            xmltree = ET.parse(frame_annotation_path)
+            f = {
+                'frame_sz': [int(it.text) for it in (xmltree.findall('size')[0])],
+                'img_path': os.path.splitext(os.path.basename(frame_annotation_path))[0] + '.JPEG',
+            }
             v['frame'].append(f)
         s.append(v)
     vid.append(s)
-print('save json (raw vid info), please wait 1 min~')
-print('total frame number: {:d}'.format(total_frame))
-json.dump(vid, open('vid.json', 'w'), indent=2)
-print('done!')
+
+print(f'Total frame number: {total_frame:d}')
+print('Writing video information, please wait...')
+
+with open(OUT_FILE, 'w') as of:
+    json.dump(vid, of, indent=2)
+
+print(f'Wrote output to {os.path.abspath(OUT_FILE)}.')
