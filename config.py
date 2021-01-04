76b3cee8d42ddf98776afb1008d32d6bdbc41120
Jannik Bamberger <dev.jbamberger@gmail.com>
2021-01-04 16:22:43 +0100

    fix broken config paths

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import torch
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-dataset_root = os.path.join('D:', 'ma', 'datasets')
-checkpoint_root = os.path.join('D:', 'ma', 'checkpoints')
-results_root = os.path.join('D:', 'ma', 'results')
+dataset_root = os.path.join('D:', os.sep, 'ma', 'datasets')
+checkpoint_root = os.path.join('D:', os.sep, 'ma', 'checkpoints')
+results_root = os.path.join('D:', os.sep, 'ma', 'results')
